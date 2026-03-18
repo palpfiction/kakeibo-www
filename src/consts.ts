a2e41915cb410af2504d0af888103a4692bcb903
Pablo Muñoz García <pablomunozgarcia@outlook.com>
2026-03-18 13:08:10 +0100

    change android beta

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,10 @@
 export const SITE_URL = "https://getkakeibo.com";
 
 export const APPLE_TESTFLIGHT_URL =
-    "https://testflight.apple.com/join/Wkz6Aht1";
+  "https://testflight.apple.com/join/Wkz6Aht1";
 
 export const ANDROID_BETA_GROUP_URL =
-    "https://groups.google.com/g/kakeibo-beta-testers/about";
+  "https://groups.google.com/g/kakeibo-beta-testers/about";
 
 export const ANDROID_BETA_PLAY_URL =
-    "https://play.google.com/apps/testing/com.palpfiction.kakeibo";
+  "https://play.google.com/apps/testing/dev.palpfiction.kakeibo";
