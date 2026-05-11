f60cc44f2c65a280f1640dc344caac77e84edbbd
palpfiction <pablomunozgarcia@outlook.com>
2026-05-11 15:17:57 +0200

    add HN landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,33 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://getkakeibo.com',
-    integrations: [
-        sitemap({
-            filter: (page) => !page.endsWith('getkakeibo.com/'),
-            i18n: {
-                defaultLocale: 'en',
-                locales: {
-                    en: 'en',
-                    es: 'es',
-                },
-            },
-        }),
-    ],
-    vite: {
-        server: {
-            watch: {
-                awaitWriteFinish: {
-                    stabilityThreshold: 150,
-                    pollInterval: 25,
-                },
-            },
+  site: "https://getkakeibo.com",
+  integrations: [
+    sitemap({
+      filter: (page) => {
+        const { pathname } = new URL(page);
+        return pathname !== "/" && pathname !== "/hn" && pathname !== "/hn/";
+      },
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en",
+          es: "es",
         },
+      },
+    }),
+  ],
+  vite: {
+    server: {
+      watch: {
+        awaitWriteFinish: {
+          stabilityThreshold: 150,
+          pollInterval: 25,
+        },
+      },
     },
+  },
 });
